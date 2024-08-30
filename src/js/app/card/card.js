d929d4610380a5c769aeccb0f8f69186dde4d0d8
Jesús <jesus.casanas@grupoiraola.com>
2024-08-30 20:11:59 -0300

    feat(styles): visual fixes card, detail button created

--- src/js/app/card/card.js
+++ src/js/app/card/card.js
@@ -7,10 +7,18 @@ const createCards = () => {
     data.map((product) => {
       const { image, title } = product;
       let card = `<div class="col" key="${title}">
-                    <div class="card" style="height: 500px">
-                      <img src=${image} class="card-img-top img-fluid object-fit-contain h-75" alt=${title}>
+                    <div class="d-flex flex-column card pt-5 pb-5" style="height: 550px">
+                      <img src=${image} class="card-img-top img-fluid object-fit-contain h-75 p-2" alt=${title} />
                       <div class="card-body text-center">
                         <h5 class="card-title">${title}</h5>
+                      </div>
+                      <div class="d-flex justify-content-center">
+                        <button
+                          type="button"
+                          class="btn btn-primary w-50"
+                        >
+                          Detalle
+                        </button>
                       </div>
                     </div>
                   </div>`;
