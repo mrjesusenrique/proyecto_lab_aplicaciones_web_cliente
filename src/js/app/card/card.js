c53981cd742c83ed5eacdfe8e2e80bd260be69b5
Facundo Benitez <fbenitez@mobydigital.com>
2024-09-27 21:08:09 -0300

    se muestra price en la card

--- src/js/app/card/card.js
+++ src/js/app/card/card.js
@@ -12,27 +12,28 @@ export const createCardElement = (product) => {
   card.setAttribute("key", title);
 
   card.innerHTML = `
-    <div class="d-flex flex-column card pt-5 pb-5" style="height: 550px">
-      <img src="${image}" class="card-img-top img-fluid object-fit-contain h-75 p-2" alt="${title}" />
-      <div class="card-body text-center">
-        <h5 class="card-title text-truncate" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${title}">
-          ${title}
-        </h5>
-      </div>
-      <div class="d-flex justify-content-center">
-        <button
-          id="btn-prod-${id}"
-          type="button"
-          class="btn btn-primary w-50 detailButton"
-          data-product-title="${title}"
-          data-product-price="${price}"
-          data-product-image="${image}"
-        >
-          Detalle
-        </button>
-      </div>
+  <div class="d-flex flex-column card pt-5 pb-5" title="${title}" style="height: 550px">
+    <img src="${image}" class="card-img-top img-fluid object-fit-contain h-75 p-2" alt="${title}" />
+    <div class="card-body text-center">
+      <h5 class="card-title text-truncate" data-bs-toggle="tooltip" data-bs-placement="bottom" >
+        ${title}
+      </h5>
+      <p class="card-text fw-bold">$${price}</p>
     </div>
-  `;
+    <div class="d-flex justify-content-center">
+      <button
+        id="btn-prod-${id}"
+        type="button"
+        class="btn btn-primary w-50 detailButton"
+        data-product-title="${title}"
+        data-product-price="${price}"
+        data-product-image="${image}"
+      >
+        Detalle
+      </button>
+    </div>
+  </div>
+`;
   return card;
 };
 
