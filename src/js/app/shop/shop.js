1b7957e7dc92186b646cbcfd10fc5b4fc409a39c
Jesús Casañas <jesusenrique9773@gmail.com>
2024-10-04 18:09:26 -0300

    fix button carrito

--- src/js/app/shop/shop.js
+++ src/js/app/shop/shop.js
@@ -2,40 +2,33 @@ import { aside } from "../aside/aside.js";
 
 export const renderButtonShop = () => {
   document.addEventListener("DOMContentLoaded", function () {
-    // Crear el botón de la tienda
     const shopButton = document.createElement("button");
-    shopButton.className = "btn-custom"; // Clase personalizada para el botón
-    shopButton.setAttribute("aria-label", "Carrito de compras"); // Añadir atributo aria-label para accesibilidad
+    shopButton.className = "btn-custom";
+    shopButton.style = "width: 1px;"
+    shopButton.setAttribute("aria-label", "Carrito de compras");
 
-    // Crear el ícono del carrito
     const iconShopButton = document.createElement("i");
-    iconShopButton.className = "fas fa-shopping-cart"; // Usar la clase Font Awesome para el ícono
+    iconShopButton.className = "fas fa-shopping-cart";
 
-    // Crear el badge de cantidad del carrito
     const cartQuantityBadge = document.createElement("span");
-    cartQuantityBadge.className = "badge bg-danger rounded-pill ms-2"; // Clase para el badge
-    cartQuantityBadge.textContent = "0"; // Inicializar cantidad en 0
+    cartQuantityBadge.className = "badge bg-danger rounded-pill ms-2";
+    cartQuantityBadge.textContent = "0";
 
-    // Agregar el ícono y el badge al botón
     shopButton.appendChild(iconShopButton);
     shopButton.appendChild(cartQuantityBadge);
 
-    // Agregar el botón al navbar
     document.querySelector("#navbar .navbar-nav").appendChild(shopButton);
 
-    // Actualizar el ícono del carrito
     updateCartIcon();
 
-    // Evento para mostrar el offcanvas al hacer clic en el botón
     shopButton.addEventListener("click", function () {
       const offcanvas = new bootstrap.Offcanvas(
         document.getElementById("offcanvasRight")
       );
       offcanvas.show();
-      aside(); // Llama a la función aside() si está definida
+      aside();
     });
 
-    // Observar cambios en el localStorage
     observeLocalStorageChange();
   });
 };
